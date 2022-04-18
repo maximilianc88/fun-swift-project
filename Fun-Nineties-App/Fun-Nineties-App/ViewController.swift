3b65be76e72ab359114c5b1022952c7876d8cf0c
Maximillian Cartwright <emillian88@gmail.com>
2022-04-18 06:39:03 -0700

    figured out basic design

---
+++ Fun-Nineties-App/Fun-Nineties-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fun-Nineties-App
+//
+//  Created by Max Cartwright on 4/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
